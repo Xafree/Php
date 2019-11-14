7aa219177c946c231d589fc59d0a4c0f84538432
Xafree <emmanueleberst@hotmail.com>
2019-11-14 12:24:16 +0100

    Mise a jour je crois que j'ai compris

---
+++ ajoutcontact.php
@@ -0,0 +1,11 @@
+<?php
+$mysqli = mysqli_connect('127.0.0.1', 'root', '', 'carnetadresse');
+
+if ($mysqli->connect_error) {
+    die('Connect Error (' . $mysqli->connect_errno . ') '
+            . $mysqli->connect_error);
+}
+echo '<p>Connection OK '. $mysqli->host_info.'</p>';
+echo '<p>Server '.$mysqli->server_info.'</p>';
+$mysqli->close();
+?>
